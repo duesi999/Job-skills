4b5d19dbc19f20bc2e7bbca466262a370781cfff
Andreas Duenser <andreas.duenser@csiro.au>
2019-12-19 16:25:04 +1100

    fixed overall comparison of Adzuna and IVI data

--- scripts/tidy and merge data.R
+++ scripts/tidy and merge data.R
@@ -5,9 +5,10 @@ library(zoo)
 
 #read in datasets
 df_Adzuna_import <- read_csv("data/Adzuna_data_v6.csv")
+df_Adzuna_occupation_GCCSA_import <- read_csv("data/Adzuna_data_v6_occupation_gccsa.csv")
 df_IVI_import <- read_xlsx("data/IVI_DATA_regional - May 2010 onwards.xlsx", sheet = 2)
 
-df_IVI <- df_IVI_import %>%
+df_IVI_occupation_GCCSA <- df_IVI_import %>%
   #select major occupational groups
   filter(ANZSCO_CODE %in% c(1, 2, 3, 4, 5, 6, 7, 8)) %>%
   #convert location to GCCSA equivalent
@@ -65,8 +66,12 @@ df_IVI <- df_IVI_import %>%
   group_by_at(vars(-IVI_count)) %>%
   summarise(IVI_count = sum(IVI_count))
   
+df_IVI <- df_IVI_occupation_GCCSA %>%
+  group_by(Month) %>%
+  summarise(IVI_count = sum(IVI_count))
 
-df_Adzuna <- df_Adzuna_import %>%
+
+df_Adzuna_occupation_GCCSA <- df_Adzuna_occupation_GCCSA_import %>%
   #merge year and month columns and turn into date
   unite(Month, c(Year,Month), sep = "-") %>%
   mutate(Month = as.Date(as.yearmon(Month))) %>%
@@ -79,17 +84,29 @@ df_Adzuna <- df_Adzuna_import %>%
   group_by(Month, GCCSA, ANZSCO_TITLE) %>%
   summarise(Adzuna_count = sum(Adzuna_count))
 
-#calculate moving average
-mav <- function(x,n=3){stats::filter(x,rep(1/n,n), sides=2)}
 
-df_Adzuna_mav <- df_Adzuna %>%
+df_Adzuna <- df_Adzuna_import %>%
+  #merge year and month columns and turn into date
+  unite(Month, c(Year,Month), sep = "-") %>%
+  mutate(Month = as.Date(as.yearmon(Month)))
+
+
+#calculate moving average
+mav <- function(x,n=3){
+  stats::filter(x,rep(1/n,n), sides=2)
+  }
+
+df_Adzuna_mav_occupation_GCCSA <- df_Adzuna_occupation_GCCSA %>%
   group_by(GCCSA, ANZSCO_TITLE) %>%
   drop_na()%>%
   mutate(Adzuna_mav_count = mav(Adzuna_count))
 
+df_Adzuna_mav <- df_Adzuna %>%
+  mutate(Adzuna_mav_count = mav(Adzuna_count))
+
     
-df_merged <- df_Adzuna_mav %>%
-  left_join(df_IVI, by = c("Month", "ANZSCO_TITLE", "GCCSA")) %>%
+df_merged_occupation_GCCSA <- df_Adzuna_mav_occupation_GCCSA %>%
+  left_join(df_IVI_occupation_GCCSA, by = c("Month", "ANZSCO_TITLE", "GCCSA")) %>%
   drop_na() %>%
   ungroup()%>%
   mutate(GCCSA = factor(GCCSA ,levels = c("Greater Sydney", "Rest of NSW", "Greater Melbourne", "Rest of Vic.", "Greater Brisbane",
@@ -97,12 +114,25 @@ df_merged <- df_Adzuna_mav %>%
                        labels = c("Greater Sydney", "Rest of NSW", "Greater Melbourne", "Rest of Vic.", "Greater Brisbane", "Rest of Qld", "Greater Adelaide", 
                                   "Rest of SA", "Greater Perth", "Rest of WA", "Greater Hobart", "Rest of Tas.", "Greater Darwin", "Rest of NT", "Australian Capital Territory")))
 
-
-df_merged_long <- df_merged %>%
+df_merged_long_occupation_GCCSA <- df_merged_occupation_GCCSA %>%
   pivot_longer(c(Adzuna_count, IVI_count, Adzuna_mav_count), names_to = "source", values_to = "count") %>%
   mutate(source = factor(source))
-    
-write_csv(df_merged, "data/jobs_data_merged.csv")
-write_csv(df_merged_long, "data/jobs_data_merged_long.csv")
 
-save(df_merged, df_merged_long, file="data/jobs_data_merged.Rdata")
+#####
+df_merged <- df_Adzuna_mav %>%
+  left_join(df_IVI, by = c("Month")) %>%
+  drop_na() %>%
+  ungroup()
+
+df_merged_long<- df_merged %>%
+  pivot_longer(c(Adzuna_count, IVI_count, Adzuna_mav_count), names_to = "source", values_to = "count") %>%
+  mutate(source = factor(source))
+
+
+write_csv(df_merged, "data/jobs_data_merged.csv")
+write_csv(df_merged, "data/jobs_data_merged_long.csv")
+write_csv(df_merged_occupation_GCCSA, "data/jobs_data_merged_occupation_GCCSA.csv")
+write_csv(df_merged_long_occupation_GCCSA, "data/jobs_data_merged_long_occupation_GCCSA.csv")
+
+
+save(df_merged, df_merged_long, df_merged_occupation_GCCSA, df_merged_long_occupation_GCCSA, file="data/jobs_data_merged.Rdata")
